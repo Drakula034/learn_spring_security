dc9541ec885dd80df67837e8535c59d6f6623451
Drakula034 <abhishek034nits@gmail.com>
2024-05-14 21:48:41 +0530

    added rest api to create new user

--- springsecuritysection-3/src/main/java/com/eazybytes/model/Customer.java
+++ springsecuritysection-3/src/main/java/com/eazybytes/model/Customer.java
@@ -3,12 +3,14 @@ package com.eazybytes.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 @Data
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int Id;
     private String email;
     private String pwd;
